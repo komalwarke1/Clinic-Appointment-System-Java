27082960fd9f8afa2fe990898873eb33e3bd581c
Komal Warake <warkekomal04@gmail.com>
2025-07-14 13:35:49 +0530

    Add Patient Module

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,34 @@
+import java.sql.DriverManager;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 public class Patient {
+    public static void addPatient(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter patient name");
+        String name=sc.nextLine();
+
+        System.out.println("Enter age");
+        int age=sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter gender");
+        String gender=sc.nextLine();
+
+        try{
+            Connection conn= DBConnection.getConnection();
+            String sql="INSERT INTO PATIENT (NAME,AGE,GENDER) VALUES (?,?,?)";
+            PreparedStatement pre=conn.prepareStatement(sql);
+            pre.setString(1,name);
+            pre.setInt(2,age);
+            pre.setString(3,gender);
+            pre.executeUpdate();
+
+            System.out.println("Patient added successfully");
+            pre.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
